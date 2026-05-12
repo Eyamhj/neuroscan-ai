42bd87223f533d9f7fd06e22eb9c61fab50752bf
Eyamhj <101055367+Eyamhj@users.noreply.github.com>
2026-05-12 09:46:39 +0100

    Remove commented lines and clean up predict.py

--- predict.py
+++ predict.py
@@ -2,16 +2,16 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.utils import load_img, img_to_array
 
-# تحميل المودال
+
 model = tf.keras.models.load_model("tumor_model.h5")
 
-# أسماء الكلاسات
+
 class_names = ['glioma', 'meningioma', 'notumor', 'pituitary']
 
 # image path
 img_path = "dataset/test/glioma/Te-gl_5.jpg"  # بدّلها باسم صورة موجودة عندك
 
-# تحميل الصورة
+
 img = load_img(img_path, target_size=(128, 128))
 img_array = img_to_array(img) / 255.0
 img_array = np.expand_dims(img_array, axis=0)
